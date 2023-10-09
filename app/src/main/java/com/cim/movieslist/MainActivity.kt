b8b80da376ae9509d78ee1c51bf2208430315c8d
TejasPatel3300 <77225883+TejasPatel3300@users.noreply.github.com>
2023-10-09 22:57:01 +0530

    main activity ui

--- app/src/main/java/com/cim/movieslist/MainActivity.kt
+++ app/src/main/java/com/cim/movieslist/MainActivity.kt
@@ -2,10 +2,20 @@ package com.cim.movieslist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cim.movieslist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setUpBottomNavigation()
+    }
+
+    private fun setUpBottomNavigation() {
+        binding?.navView?.menu?.getItem(2)?.isEnabled = false
     }
 }
